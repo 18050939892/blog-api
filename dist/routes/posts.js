9cf257f81efd6e928ae58adda4706c8061984188
18050939892 <deerkesi3815@gmail.com>
2025-04-23 01:50:57 +0800

    Merge remote-tracking branch 'origin/master'

--- dist/routes/posts.js
+++ dist/routes/posts.js
@@ -38,5 +38,11 @@ router.put('/comment', (req, res) => __awaiter(void 0, void 0, void 0, function*
     const { username, content, id } = req.body;
     const post = yield Post_1.default.findById(id);
     post === null || post === void 0 ? void 0 : post.comments.push({ user: username, content });
+    if (post) {
+        res.json(post);
+    }
+    else {
+        res.json({ username, content, id });
+    }
 }));
 exports.default = router;
